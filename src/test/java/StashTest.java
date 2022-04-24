591e0b23cb8de965ca8c353bdc176e294dc1eef5
Yamali23 <den.yamali@yandex.ru>
2022-04-24 15:52:40 +0400

    Uploaded StashTest

--- src/test/java/StashTest.java
+++ src/test/java/StashTest.java
@@ -9,13 +9,25 @@ public class StashTest {
 	@Test
 	public void test() throws IOException {
 		
-		Stash st = new Stash("height", "width", " NumStvor", "NumFuncStvor", "TypeStvor", "PDF");
+		Stash st = new Stash("1.2", "2.3", "1", "1", "1", "PDF");
 		double cost = 0;
-		//cost = 123;
 		
 	    cost = st.calculator(2.1,2.1,1,1,1,1);
-		//System.out.println(cost);
 	    assertTrue(cost > 0);
+	    
+	    String s;
+	    s = st.getA();
+	    assertTrue(s.length() > 0);
+	    s = st.getB();
+	    assertTrue(s.length() > 0);
+	    s = st.getZ();
+	    assertTrue(s.length() > 0);
+	    s = st.getY();
+	    assertTrue(s.length() > 0);
+	    s = st.getD();
+	    assertTrue(s.length() > 0);
+	    
+	    
 	    
 	}
 
