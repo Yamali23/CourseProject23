a2f4bd9fb4c34a05db0ff9a531bf2dcf7ccc67fb
Ayupov23 <donatpalpatin@mail.ru>
2022-05-03 20:02:24 +0400

    Updated CreatePDFTest

--- src/test/java/start/CreatePDFTest.java
+++ src/test/java/start/CreatePDFTest.java
@@ -1,7 +1,13 @@
 package start;
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 import org.junit.Test;
@@ -9,7 +15,7 @@ import org.junit.Test;
 public class CreatePDFTest {
 
 	@Test
-	public void test() {
+	public void test() throws IOException {
 	//double[] price, String[] coeff,double a,  double b, int z, int y, int d, double finalprice
 		
 		
@@ -20,14 +26,23 @@ public class CreatePDFTest {
 		double[] quant = new double[19];
 		
 
-		ClassLoader classLoader = getClass().getClassLoader();
-	    InputStream myFile = classLoader.getResourceAsStream("coeffs");
-	    Scanner scanner = new Scanner(myFile);
-	   
-	  
+		String filepath = new File("").getCanonicalPath();
+		String[] parsfilepath = filepath.split("/");
+		int lengthpath = parsfilepath.length;
+		String abspath = "";
+		for (int ii = 0; ii < (lengthpath - 1); ii++) {
+			abspath = abspath + parsfilepath[ii] + "/";
+		}
+		filepath = abspath + "webapps/WindowsCalculator/coeffs";
+		
+		File file = new File(filepath);
+		FileInputStream fis = new FileInputStream(file);
+		InputStreamReader isr = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(isr);
+		
 	   for (int i = 1; i  <=18; i ++)
 	    {
-		   coeff[i] = scanner.nextLine();
+		   coeff[i] = br.readLine();
 		   price[i] = Math.random() * (500 - 1) + 1;
 	    }
 		       
